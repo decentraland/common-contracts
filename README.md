b3c27d6a6708871e75ce7f4b389dec063a945f9c
Nando <24811313+fzavalia@users.noreply.github.com>
2022-06-10 15:41:16 -0300

    feat: Setup Project

---
+++ README.md
@@ -0,0 +1,14 @@
+# Contracts Commons
+
+Abstract contracts to be used and reused on different projects.
+
+## Contracts
+
+- NativeMetaTransaction
+- NonceVerifiable
+
+## Running Tests
+
+Install dependencies with `npm ci`
+
+Run tests with `npx hardhat test`
---
+++ hardhat.config.ts
@@ -0,0 +1,16 @@
+import '@typechain/hardhat'
+import '@nomiclabs/hardhat-waffle'
+import 'solidity-coverage'
+import 'hardhat-gas-reporter'
+
+export default {
+  solidity: {
+    version: '0.8.7',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+}

93a3471df9e53b11bf2f36fc8139f8ae8f99b34e
nando <24811313+fzavalia@users.noreply.github.com>
2022-08-04 17:37:57 -0300

    feat: Separate nonce verifiers (#16)
    * feat: Separate nonce verifiers
    
    * feat: Delete NonceVerifiable
    
    * chore: Update README
    
    * feat: Update events

---
+++ contracts/signatures/ContractNonceVerifiable.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+abstract contract ContractNonceVerifiable is OwnableUpgradeable {
+    /// @notice Current nonce at a contract level. Only updatable by the owner of the contract.
+    /// Updating it will invalidate all signatures created with the previous value on a contract level.
+    uint256 private contractNonce;
+
+    event ContractNonceUpdated(uint256 _newNonce, address _sender);
+
+    function __ContractNonceVerifiable_init() internal onlyInitializing {
+        __Ownable_init();
+    }
+
+    function __ContractNonceVerifiable_init_unchained() internal onlyInitializing {}
+
+    /// @notice Get the current contract nonce.
+    /// @return The current contract nonce.
+    function getContractNonce() external view returns (uint256) {
+        return contractNonce;
+    }
+
+    /// @notice As the owner of the contract, increase the contract nonce by 1.
+    function bumpContractNonce() external onlyOwner {
+        _bumpContractNonce();
+    }
+
+    /// @dev Increase the contract nonce by 1
+    function _bumpContractNonce() internal {
+        emit ContractNonceUpdated(++contractNonce, _msgSender());
+    }
+
+    /// @dev Reverts if the provided nonce does not match the contract nonce.
+    function _verifyContractNonce(uint256 _nonce) internal view {
+        require(_nonce == contractNonce, "ContractNonceVerifiable#_verifyContractNonce: CONTRACT_NONCE_MISMATCH");
+    }
+}
